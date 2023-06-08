e28c873b11973caca26ddfe81b497215cb72e8f7
Omar Alazzawi <omar.alazzawi@iths.se>
2023-06-08 11:17:21 +0200

    Add project files.

---
+++ labb1pp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace labb1pp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
